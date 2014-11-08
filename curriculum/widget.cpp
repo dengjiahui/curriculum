6de07d4ce5134606a908c84a2108cfcb73009acc
qyl <605861246@qq.com>
2014-11-08 18:39:50 +0800

    Post request. Add the request to HFUT but not finished.

--- curriculum/widget.cpp
+++ curriculum/widget.cpp
@@ -2,13 +2,18 @@
 #include "widget.h"
 #include <QPainter>
 #include <QHBoxLayout>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
 #include <QFile>
 #include <utility>
+
+const QString HFUT_URL("http://210.45.240.29/");
 
 widget::widget(QWidget* parent): QWidget(parent), ui(new Ui::login)
 {
 	ui->setupUi(this);
-//	qApp->setStyleSheet(load_qss("login"));
+	manager = new QNetworkAccessManager(this);
 }
 
 widget::~widget()
@@ -17,6 +22,7 @@ widget::~widget()
 	ui = nullptr;
 }
 
+// paint the login background
 void widget::paintEvent(QPaintEvent*)
 {
 	QPainter painter(this);
@@ -26,6 +32,7 @@ void widget::paintEvent(QPaintEvent*)
 	painter.drawPixmap(this->rect(), login_picture);
 }
 
+// load a qss file
 QString widget::load_qss(const QString& file_name) const
 {
 	QFile file(":/qss/" + file_name + "_qss");
@@ -38,4 +45,42 @@ QString widget::load_qss(const QString& file_name) const
 	file.close();
 
 	return std::move(style_sheet);
+}
+
+// the login action
+void widget::on_button_login_clicked()
+{
+	auto id = ui->line_edit_id->text();
+	auto passwd = ui->line_edit_pwd->text();
+
+	connect(reply, &QNetworkReply::finished,
+		this, &widget::request_finished);
+	login(id, passwd);
+}
+
+// deal with the reply
+void widget::request_finished()
+{
+	if (QNetworkReply::NoError == reply->error()) {
+		information = new QFile(":/info/main_info");
+		if (information->open(QFile::WriteOnly)) {
+			information->write(reply->readAll());
+		}
+		information->close();
+		delete information;
+		information = nullptr;
+	} else {
+		// leave to complete later
+	}
+	reply->deleteLater();
+	reply = nullptr;
+}
+
+// post a request and save the returned result
+void widget::login(const QString& id, const QString& passwd)
+{
+	url = std::move(QUrl(HFUT_URL + "pass.asp"));
+	QByteArray info("user=" + id + "&password=" + passwd);
+
+	reply = manager->post(QNetworkRequest(url), info);
 }
--- curriculum/widget.h
+++ curriculum/widget.h
@@ -2,10 +2,15 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QUrl>
 
 namespace Ui {
 	class login;
 }
+
+class QNetworkReply;
+class QNetworkAccessManager;
+class QFile;
 
 class widget: public QWidget {
 	Q_OBJECT
@@ -14,10 +19,18 @@ public:
 	~widget();
 protected:
 	void paintEvent(QPaintEvent *) override;
+private slots:
+	void on_button_login_clicked();
 private:
+	void request_finished();
+	void login(const QString& id, const QString& passwd);
 	QString load_qss(const QString& file_name) const;
 
-	Ui::login* ui = nullptr;
+	QUrl url;
+	QNetworkReply* reply = nullptr;
+	QNetworkAccessManager* manager = nullptr;
+	QFile* information = nullptr;
+	Ui::login* ui = nullptr;	
 };
 
 #endif // WIDGET_H
