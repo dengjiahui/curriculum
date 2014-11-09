c2d7a56e55515f9deef252d4ad1320806e49f332
qyl <605861246@qq.com>
2014-11-09 17:30:36 +0800

    Able to get info. ContentTypeHeader was set wrong. Now it can connect to the url.

--- curriculum/widget.cpp
+++ curriculum/widget.cpp
@@ -5,6 +5,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QNetworkRequest>
+#include <QUrlQuery>
 #include <QFile>
 #include <utility>
 #include <QDebug>
@@ -54,7 +55,7 @@ void widget::on_button_login_clicked()
 	auto id = ui->line_edit_id->text();
 	auto passwd = ui->line_edit_pwd->text();
 
-	information = new QFile("a.txt");
+	information = new QFile("index.html");
 	if (!information->open(QIODevice::WriteOnly)) {
 		qDebug() <<  "file open error";
 		delete information;
@@ -65,34 +66,73 @@ void widget::on_button_login_clicked()
 	login(id, passwd);
 }
 
-// deal with the reply
+// do the clean work
 void widget::request_finished()
 {
 	information->flush();
 	information->close();
+	QVariant redirectionTarget =
+		reply->attribute(QNetworkRequest::RedirectionTargetAttribute);
+
+	if (QNetworkReply::NoError == reply->error()) {
+		if (!redirectionTarget.isNull()) {
+			url = url.resolved(redirectionTarget.toUrl());
+			reply->deleteLater();
+			information->open(QIODevice::WriteOnly);
+			information->resize(0);
+			start_get();
+
+			return;
+		}
+	}
 	reply->deleteLater();
 	reply = nullptr;
 	delete information;
 	information = nullptr;
+	information = new QFile("index.html");
 }
 
-void widget::request_ready() const noexcept
+// write returned information into the file
+void widget::request_ready() noexcept
 {
 	if (information) {
-		information->write(reply->readAll());
+		information->write((reply->readAll()));
 	}
 }
 
-// post a request and save the returned result
+// post a request and prepare for the result
 void widget::login(const QString& id, const QString& passwd)
 {
 	url = std::move(QUrl(HFUT_URL + "pass.asp"));
-	QByteArray info(
-		QString(
-		"user=" + id + "&password=" + passwd).toStdString().c_str());
+	QUrlQuery params;
 
-//	reply = manager->post(QNetworkRequest(url), info);
-	reply = manager->get(QNetworkRequest(QUrl("http://www.baidu.com")));
-	connect(reply, SLOT(readyRead()), this, SLOT(request_ready()));
-	connect(reply, SLOT(finished()), this, SLOT(request_finished()));
+	params.addQueryItem("UserStyle", "student");
+	params.addQueryItem("user", id);
+	params.addQueryItem("password", passwd);
+
+	start_post(params);
+//	start_get();
+}
+
+// the connect part using post way
+void widget::start_post(const QUrlQuery& params) noexcept
+{
+	QNetworkRequest request(url);
+
+	request.setHeader(QNetworkRequest::ContentTypeHeader,
+			  QVariant("application/x-www-form-urlencoded"));
+	reply = manager->post(request,
+			      params.query(QUrl::FullyEncoded).toUtf8());
+
+	connect(reply, SIGNAL(readyRead()), this, SLOT(request_ready()));
+	connect(reply, SIGNAL(finished()), this, SLOT(request_finished()));
+}
+
+// the get way
+void widget::start_get() noexcept
+{
+	reply = manager->get(QNetworkRequest(url));
+
+	connect(reply, SIGNAL(readyRead()), this, SLOT(request_ready()));
+	connect(reply, SIGNAL(finished()), this, SLOT(request_finished()));
 }
--- curriculum/widget.h
+++ curriculum/widget.h
@@ -10,6 +10,7 @@ namespace Ui {
 
 class QNetworkAccessManager;
 class QNetworkReply;
+class QUrlQuery;
 class QFile;
 
 class widget: public QWidget {
@@ -21,9 +22,11 @@ protected:
 	void paintEvent(QPaintEvent *) override;
 private slots:
 	void on_button_login_clicked();
-private:
 	void request_finished();
-	void request_ready() const noexcept;
+	void request_ready() noexcept;
+	void start_post(const QUrlQuery& params) noexcept;
+	void start_get() noexcept;
+private:
 	void login(const QString& id, const QString& passwd);
 	QString load_qss(const QString& file_name) const;
 
@@ -31,7 +34,7 @@ private:
 	QNetworkReply* reply = nullptr;
 	QNetworkAccessManager* manager = nullptr;
 	QFile* information = nullptr;
-	Ui::login* ui = nullptr;	
+	Ui::login* ui = nullptr;
 };
 
 #endif // WIDGET_H
